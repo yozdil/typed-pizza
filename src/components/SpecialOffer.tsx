8ae91b3d45dc7414fb0c5707c7f4eb28306d852d
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-17 12:07:45 -0400

    add special offer functionality

--- src/components/SpecialOffer.tsx
+++ src/components/SpecialOffer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import PizzaCSS from "./Pizza.module.css";
-import { useStateDispatch } from "./AppState";
 import { Pizza } from "../types";
+import { useStateDispatch } from "./AppState";
+import SpecialOfferCSS from "./SpecialOffer.module.css";
 
 interface Props {
   pizza: Pizza;
 }
 
-const PizzaItem: React.FC<Props> = ({ pizza }) => {
+const SpecialOffer: React.FC<Props> = ({ pizza }) => {
   const dispatch = useStateDispatch();
   const handleAddToCartClick = () => {
     dispatch({
@@ -18,15 +18,15 @@ const PizzaItem: React.FC<Props> = ({ pizza }) => {
     });
   };
   return (
-    <li className={PizzaCSS.container}>
+    <div className={SpecialOfferCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
       <button type="button" onClick={handleAddToCartClick}>
         Add to Cart
       </button>
-    </li>
+    </div>
   );
 };
 
-export default PizzaItem;
+export default SpecialOffer;
