da692b9f186bd5009a224507454fdd6cad14f37f
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-17 09:38:03 -0400

    wrap App in Context Provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import pizzas from "../data/pizzas.json";
 import Pizza from "./Pizza";
+import Cart from "./Cart";
 import AppCSS from "./App.module.css";
 import PizzaSVG from "../svg/pizza.svg";
-import Cart from './Cart'
+import AppStateProvider from "./AppState";
 
 const App = () => {
   return (
-    <div className={AppCSS.container}>
-      <div className={AppCSS.header}>
-        <PizzaSVG width={120} height={120} />
-        <div className={AppCSS.siteTitle}>Typed Pizza</div>
-        <Cart />
+    <AppStateProvider>
+      <div className={AppCSS.container}>
+        <div className={AppCSS.header}>
+          <PizzaSVG width={120} height={120} />
+          <div className={AppCSS.siteTitle}>Typed Pizza</div>
+          <Cart />
+        </div>
+        <ul>
+          {pizzas.map((pizza) => {
+            return <Pizza key={pizza.id} pizza={pizza} />;
+          })}
+        </ul>
       </div>
-      <ul>
-        {pizzas.map((pizza) => {
-          return <Pizza key={pizza.id} pizza={pizza} />;
-        })}
-      </ul>
-    </div>
+    </AppStateProvider>
   );
 };
 
