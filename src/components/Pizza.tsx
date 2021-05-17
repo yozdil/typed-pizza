7f5baf994790f51e0bbbbabbed986a7e3a200e24
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-17 09:49:45 -0400

    implement state with function handleAddToCartClick

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import PizzaCSS from './Pizza.module.css'
+import PizzaCSS from "./Pizza.module.css";
+import { useSetState } from "./AppState";
 
 interface Pizza {
   id: number;
@@ -13,11 +14,29 @@ interface Props {
 }
 
 const Pizza: React.FC<Props> = ({ pizza }) => {
+  const setState = useSetState();
+  const handleAddToCartClick = () => {
+    setState((state) => {
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          items: [
+            ...state.cart.items,
+            { id: pizza.id, name: pizza.name, price: pizza.price },
+          ],
+        },
+      };
+    });
+  };
   return (
     <li className={PizzaCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
+      <button type="button" onClick={handleAddToCartClick}>
+        Add to Cart
+      </button>
     </li>
   );
 };
